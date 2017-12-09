9ae5fc21964ebf2cb4994faf7bab7d4905ca7464
Olga Svalova <harlozavr@gmail.com>
2017-12-10 02:10:30 +0700

    Домашка 3, часть 2

---
+++ Selenium_Training_1/Homework_3_2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Chrome;
+using System.Collections.Generic;
+using OpenQA.Selenium.Support.UI;
+
+namespace Selenium_Training_1
+{
+
+    [TestFixture]
+    public class ClassHomework_3_2
+    {
+        private EdgeDriver driver;
+
+        [SetUp]
+        public void start()
+        {
+            driver = new EdgeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(3);
+        }
+
+        [Test]
+        public void Homework_3_2()
+        {
+            driver.Url = "http://localhost:9999/";
+            IList <IWebElement> products = driver.FindElements(By.ClassName("product"));
+            for (int i = 0; i < products.Count; i++)
+            {
+                IList<IWebElement> stickers = products[i].FindElements(By.ClassName("sticker"));
+                Assert.True(stickers.Count == 1);
+                Console.WriteLine(i + ". " + stickers[0].Text);
+            }
+        }
+
+        [TearDown]
+        public void stop()
+        {
+            driver.Quit();
+            driver = null;
+        }
+    }
+}
